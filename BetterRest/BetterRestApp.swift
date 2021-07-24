66a8ac50dffbcf3d9eafd86d2836532b8c8511fa
Promal B <promal.barua.cse@gmail.com>
2021-07-24 18:36:05 +0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Promal on 24/7/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
